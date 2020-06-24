ee24312050e56a5a05a1358d844a86c25dda0f24
Wu-Patrick <37534299+Wu-Patrick@users.noreply.github.com>
2020-06-24 14:43:41 +0800

    Update getDownloadBash.sh

--- getDownloadBash.sh
+++ getDownloadBash.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-wget https://github.com/Wu-Patrick/first/raw/master/download.sh
+wget https://github.com/Wu-Patrick/first/raw/master/download.py
 
 echo "download.sh has been updated!"
